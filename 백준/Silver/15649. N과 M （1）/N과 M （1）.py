536f9c77453ea84f0f34a7c976b6f94bb1ed4508
AngelPlayer <ssh5212@naver.com>
2024-02-01 15:27:21 +0900

    [Silver III] Title: N과 M (1), Time: 200 ms, Memory: 114364 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,25 @@
+global arr, sel, v, n, m
+
+def recursive(count):
+    if count == m:
+        print(*sel)
+        return
+
+    for i in range(n):
+        if v[i] == True: continue
+        v[i] = True
+        sel[count] = arr[i]
+        recursive(count + 1)
+        sel[count] = 0
+        v[i] = False
+
+def main():
+    global n, m, arr, sel, v
+    n, m = map(int, input().split(" "))
+    arr = [i for i in range(1, n + 1)]
+    v = [False] * n
+    sel = [0] * m
+
+    recursive(0)
+
+main()
