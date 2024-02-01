b639d92956c137eaf97f8491431a2e5ea3050b0e
AngelPlayer <ssh5212@naver.com>
2024-02-01 15:45:47 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,27 @@
+global arr, sel, v, n, m
+
+def recursive(count, index):
+    if count == m:
+        for i in sel:
+            print(i, end=" ")
+        print()
+        return
+
+    for i in range(index, n):
+        # if v[i] == True: continue
+        # v[i] = True
+        sel[count] = arr[i]
+        recursive(count + 1, i + 1)
+        sel[count] = 0
+        # v[i] = False
+
+def main():
+    global n, m, arr, sel, v
+    n, m = map(int, input().split(" "))
+    arr = [i for i in range(1, n + 1)]
+    # v = [False] * n
+    sel = [0] * m
+
+    recursive(0, 0)
+
+main()
