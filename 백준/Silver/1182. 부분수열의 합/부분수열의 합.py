38c64ef8cd430068543983d8f6f0538132b3ec2e
AngelPlayer <ssh5212@naver.com>
2024-02-07 13:25:16 +0900

    [Silver II] Title: 부분수열의 합, Time: 248 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,37 +1,32 @@
-global n, s, arr, v, answer
+global N, S, arr, answer
 
 
-def recursive(count):
-    global n, s, arr, v, answer
+def recursive(index, total):
+    global N, S, arr, answer
 
-    if count == n:
-        nowAns = 0
-        count = 0
-        for i in range(n):
-            if v[i] == True:
-                nowAns += arr[i]
-                count += 1
-        if nowAns == s and count != 0:
+    if index == N:
+        if total == S:
             answer += 1
-
         return
 
-    v[count] = True
-    recursive(count + 1)
-    v[count] = False
-    recursive(count + 1)
+    recursive(index + 1, total + arr[index])
+    recursive(index + 1, total)
 
 
 def main():
-    global n, s, arr, v, answer
+    global N, S, arr, answer
+    N, S = map(int, input().split())
 
-    n, s = map(int, input().split())
     arr = list(map(int, input().split()))
-    v = [False] * n
+
     answer = 0
 
-    recursive(0)
-    print(answer)
+    recursive(0, 0)
+
+    if S == 0:
+        print(answer - 1)
+    else:
+        print(answer)
 
 
 main()
